5c531febcbdf12a472c9e1f372ca489feffbeeb8
Joshua Caron <josh@tubedepot.com>
2012-01-23 09:46:12 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProductApp::Application.config.secret_token = '7e38a7baf4161cabbb7b0db03d5e21683b60623d620453c6cba833c71de8c2fe9c001d015d2a892ddfad6af1d3c1a3d702e7422608081abef5e1dc250b6ec00c'
